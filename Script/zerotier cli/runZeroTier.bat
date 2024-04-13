b90b08f2ea29bbf8fe4735094abb4a7af6f29187
JordanSihombing <70830660+JordanSihombing@users.noreply.github.com>
2024-04-13 20:55:08 +0700

    Update runZeroTier.bat

--- Script/zerotier cli/runZeroTier.bat
+++ Script/zerotier cli/runZeroTier.bat
@@ -26,6 +26,24 @@ if '%errorlevel%' NEQ '0' (
 
 echo Initiating ZeroTier connection...
 
+:: Function to send request to API and extract ID from response
+:: Define API endpoint URL 
+set TARGET=http://IP_FOR_API/getid
+
+:: Execute GET request with PowerShell and capture response
+for /f "tokens=2 delims=:" %%A in ('powershell -Command "Invoke-RestMethod -Uri '%TARGET%' | ConvertTo-Json -Compress") do set "response=%%A"
+
+:: Extract ID from JSON response 
+set "network_id="
+for /f "tokens=2 delims=:" %%A in ('echo.%response% | findstr /i /c:"id"') do set "id=%%B"
+
+rem Check if ID extraction was successful
+if defined network_id (
+  echo Extracted ID: %network_id%
+) else (
+  echo Error: Could not extract ID from response.
+)
+
 :: Check for ZeroTier Desktop UI executable
 set "ZerotierExe="
 for %%i in ("%ProgramFiles%\ZeroTier\One\zerotier_desktop_ui.exe" "%ProgramFiles(x86)%\ZeroTier\One\zerotier_desktop_ui.exe") do (
@@ -38,11 +56,11 @@ for %%i in ("%ProgramFiles%\ZeroTier\One\zerotier_desktop_ui.exe" "%ProgramFiles
 echo ZeroTier Desktop UI not found.
 goto :EndScript
 
-::Initiating ZeroTier and connect to the network 
+:: Initiating ZeroTier and connect to the network 
 :StartZerotier
 start "" "%ZerotierExe%"
 timeout /t 5 /nobreak
-zerotier-cli join 41d49af6c2cdaccf  REM --> change the network ID to corresponding network ID
+zerotier-cli join %network_id%
 timeout /t 3 /nobreak
 zerotier-cli status
 echo ZeroTier connection established!
