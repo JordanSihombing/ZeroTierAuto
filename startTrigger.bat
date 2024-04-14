27e50e7785b33ce00bf7d999844481a7db2a0df9
JordanSihombing <70830660+JordanSihombing@users.noreply.github.com>
2024-04-14 17:07:26 +0700

    Create startTrigger.bat

---
+++ startTrigger.bat
@@ -0,0 +1,7 @@
+@echo off
+
+:: Run the scripts one by one
+call Script/zerotier api/createNew.sh
+call Script/zerotier api/editNet.sh
+start Script/zerotier cli/startCall.bat
+call Script/zerotier api/sendPIN.sh
