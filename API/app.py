896079d9fa4c97b8717ad5db6ae845a05980bbb3
JordanSihombing <jordansihombing17@gmail.com>
2024-04-22 21:47:54 +0700

    ll

--- API/app.py
+++ API/app.py
@@ -1,5 +1,4 @@
 from flask import Flask, request, jsonify
-import json
 
 app = Flask(__name__)
 
@@ -8,32 +7,32 @@ received_pin = []
 received_id = []
 
 @app.route('/getid', methods=['GET'])
-def handle_get_request():
+def handle_get_id_request():
     if len(received_id) > 0:
-        last_pin = received_pin[-1]
-        return jsonify({'id': last_pin}), 200
+        last_id = received_id[-1]
+        return jsonify({'id': last_id}), 200
     else:
         return jsonify({'id': 'No available ID'}), 400
     
 @app.route('/getpin', methods=['GET'])
-def handle_get_request():
+def handle_get_pin_request():
     if len(received_pin) > 0:
         last_pin = received_pin[-1]
-        return jsonify({'id': last_pin}), 200
+        return jsonify({'pin': last_pin}), 200
     else:
         return jsonify({'pin': 'No available PIN'}), 400
 
 @app.route('/sendid', methods=['POST'])
-def handle_post_request():
-    pin = request.json.get('id')
-    if pin is not None:
-        received_pin.append(pin)
+def handle_post_id_request():
+    id_number = request.json.get('id')
+    if id_number is not None:
+        received_id.append(id_number)
         return {'status': 'success'}, 200
     else:
         return {'status': 'error', 'message': 'ID number not provided'}, 400
 
 @app.route('/sendpin', methods=['POST'])
-def handle_post_request():
+def handle_post_pin_request():
     pin = request.json.get('pin')
     if pin is not None:
         received_pin.append(pin)
@@ -42,4 +41,4 @@ def handle_post_request():
         return {'status': 'error', 'message': 'PIN not provided'}, 400
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=2020)
