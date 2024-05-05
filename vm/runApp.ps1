3f717d3e4807b210da0a336e2bbf90980d29a4fa
JordanSihombing <jordansihombing17@gmail.com>
2024-05-05 13:48:33 +0700

    API changed to .exe

--- vm/runApp.ps1
+++ vm/runApp.ps1
@@ -1,11 +1,5 @@
-# Define the path to your Python executable
-$python_exe = "python"
+# Define the path to your executable
+$app_exe = ".\API\dist\app.exe"
 
-# Define the path to your Flask application file
-$flask_app = "app.py"
-
-# Build the command to run the Flask app
-$command = "$python_exe $flask_app"
-
-# Execute the command
-Invoke-Expression $command
+# Execute the executable
+Start-Process -FilePath $app_exe
