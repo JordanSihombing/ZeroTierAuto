d10f9e7b3aaf9967835e3fefa8206d6ed0d56974
JordanSihombing <jordansihombing17@gmail.com>
2024-05-09 15:22:55 +0700

    vmStart update

--- controller/vmStart.ps1
+++ controller/vmStart.ps1
@@ -1,4 +1,18 @@
 & "API\dist\app.exe" #start API
+
+# Stop the ZeroTier service
+Stop-Service -Name ZeroTierService
+
+# Define the path to ZeroTier's working directory
+$zerotierDir = "C:\ProgramData\ZeroTier\One"
+
+# Delete the files identity.public and identity.secret
+Remove-Item -Path "$zerotierDir\identity.public" -Force
+Remove-Item -Path "$zerotierDir\identity.secret" -Force
+
+# Start the ZeroTier service
+Start-Service -Name ZeroTierService
+
 .\vm\createNew.ps1 #Create new network
 .\warp\counterEduroam.ps1 #Check if network is restricted 
 Start-Sleep -Seconds 5
