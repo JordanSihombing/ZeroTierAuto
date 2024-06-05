874101b6614361dc6968929c334907bd321b0b82
JordanSihombing <jordansihombing17@gmail.com>
2024-06-05 15:33:20 +0700

    fix sunshine problem

--- vm/runSunshine.ps1
+++ vm/runSunshine.ps1
@@ -29,8 +29,11 @@ if ($SunshineProcess) {
 }
 
 # Start Sunshine
+$workingDirectory = Split-Path -Path $SunshineExe -Parent
+Write-Host "Setting working directory to $workingDirectory"
+Set-Location -Path $workingDirectory
 Write-Host "Starting Sunshine..."
-Start-Process -FilePath $SunshineExe
+Start-Process -FilePath $SunshineExe -NoNewWindow -Wait
 
 # Wait for Sunshine to start
 Start-Sleep -Seconds 5
