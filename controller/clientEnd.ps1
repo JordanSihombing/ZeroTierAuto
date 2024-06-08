c948536ac706fc9b46ccb4e3594c1344af9f3c2c
JordanSihombing <jordansihombing17@gmail.com>
2024-06-08 17:32:42 +0700

    path fix

--- controller/clientEnd.ps1
+++ controller/clientEnd.ps1
@@ -2,7 +2,7 @@ param (
     [string]$network_id
 )
 
-.\client\disconnectClient.ps1 -network_id $network_id
+. ..\client\disconnectClient.ps1 -network_id $network_id
 .\warp\warpDisconnect.ps1
 
 #Show-Notification -Message "Connection terminated successfully!"
