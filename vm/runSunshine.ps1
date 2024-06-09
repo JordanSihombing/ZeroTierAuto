40482c04026adfe0db99563740fb7739145f8cad
JordanSihombing <jordansihombing17@gmail.com>
2024-06-09 14:34:57 +0700

    update

--- vm/runSunshine.ps1
+++ vm/runSunshine.ps1
@@ -1,6 +1,7 @@
 $SunshineCheck = "C:\Program Files\Sunshine\sunshine.exe"
 $SunshinePath = "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Sunshine\sunshine.lnk"
 
+# Check if Sunshine is installed
 if (-not (Test-Path $SunshineCheck)) {
     Write-Host "Sunshine is not installed."
     exit
@@ -8,18 +9,23 @@ if (-not (Test-Path $SunshineCheck)) {
 
 Write-Host "Sunshine is installed."
 
-#start Sunshine
-$workDir =Split-Path -Path $SunshinePath -Parent
+# Set working directory to the directory containing the Sunshine shortcut
+$workDir = Split-Path -Path $SunshinePath -Parent
 Write-Host "Setting working directory to $workDir"
 Set-Location -Path $workDir
 
-$shell =  New-Object -ComObject WScript.Shell
+# Create WScript.Shell COM object
+$shell = New-Object -ComObject WScript.Shell
 
+# Create shortcut object
 $Shortcut = $shell.CreateShortcut($SunshinePath)
-$TargetPath = $Shortcut.$TargetPath
+$TargetPath = $Shortcut.TargetPath
 
+# Start Sunshine
+Write-Host "Starting Sunshine from $TargetPath"
 Start-Process -FilePath $TargetPath -NoNewWindow -Wait
 
 Start-Sleep -Seconds 5
 
+Write-Host "Sunshine started successfully."
 exit
