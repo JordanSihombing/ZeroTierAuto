aec8ef3522228daa96fd2b6db9115a36798ebff2
JordanSihombing <jordansihombing17@gmail.com>
2024-06-10 15:50:28 +0700

    IP handling

--- textbox.py
+++ textbox.py
@@ -2,15 +2,29 @@ import tkinter as tk
 import requests
 import json
 import sys
+import subprocess
 
 global session_id
 global token
 
-def sendPIN (pin):
+def ping(host):
+    try:
+        output = subprocess.check_output(["ping", "-c", "1", host])
+        return True
+    except subprocess.CalledProcessError:
+        return False
+
+def sendPIN(pin):
     if pin:
         print(session_id)
-        # Set the URL
-        URL = f"http://10.11.1.181:3000/v1/session/{session_id}/pair"
+        # Ping destinations and set URL
+        if ping("10.11.1.181"):
+            URL = f"http://10.11.1.181:3000/v1/session/{session_id}/pair"
+        elif ping("10.147.20.105"):
+            URL = f"http://10.147.20.105:3000/v1/session/{session_id}/pair"
+        else:
+            print("Error: No destination is reachable.")
+            return
 
         # Create JSON body with PIN value
         body = {
@@ -41,7 +55,6 @@ def get_input():
     sendPIN(user_input)
     
 
-
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         session_id = sys.argv[1]
@@ -55,7 +68,7 @@ if __name__ == "__main__":
         label.pack()
 
         # Create an entry widget (textbox) with a wider width
-        entry = tk.Entry(root, width=40)  # Set the width to 50 characters
+        entry = tk.Entry(root, width=40)  # Set the width to 40 characters
         entry.pack()
 
         # Create a button to submit the input
@@ -66,7 +79,3 @@ if __name__ == "__main__":
         root.mainloop()
     else:
         exit("No session ID provided")
-
-
-
-
